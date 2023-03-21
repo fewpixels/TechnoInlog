3411b8dc79a05b3dcc808cb6997f0fb2020229ab
Melissa Blankenburgh <melissablankenburgh@gmail.com>
2023-03-21 14:00:20 +0100

    de header is nu een stuk meer reliable
    i heb de url van de header veranderd naar alleen het document wat we nodig hebben

--- QRLogin/data_lookup.php
+++ QRLogin/data_lookup.php
@@ -6,7 +6,7 @@ if(isset($_GET['data'])) {
     echo 'beste <b>' . $_GET['data'] . '</b> je bent ingelogd!'; 
     //nu dus ervoor zorgen dat het de gebruiker ophaalt en hier weergeeft ipv echo lol
 
-    header( "refresh:2; url=http://localhost/github/TechnoInlog/QRLogin/" ); 
+     header( "refresh:2; url=index.html" ); 
 }
 
 ?>
