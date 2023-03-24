8d6b0e28501b0eaa2e9bbdf7badfecd82c4105b6
Melissa Blankenburgh <melissablankenburgh@gmail.com>
2023-03-24 13:18:46 +0100

    styling gedaan
    ik heb gezorgd dat de styling overeenkomt met de wireframes. ook heb ik wat wordt ge-echo'd in een h1 gezet. Ik wou eerst dingen doen met een grid, maar ik had besloten dat dit toch een stuk makkelijker is.

--- QRLogin/data_lookup.php
+++ QRLogin/data_lookup.php
@@ -1,12 +1,29 @@
+<head>
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+    
+<article id='main'>
+    <section id='filler'>
+
+    </section>
+    <section id='text'>
+        
 <?php
 
 require_once 'backend/classes/DBConfig.php';
 
 if(isset($_GET['data'])) {
-    echo 'beste <b>' . $_GET['data'] . '</b> je bent ingelogd!'; 
+    echo '<h1>beste ' . $_GET['data'] . ' je bent ingelogd!</h1>'; 
     //nu dus ervoor zorgen dat het de gebruiker ophaalt en hier weergeeft ipv echo lol
 
      header( "refresh:2; url=index.html" ); 
 }
 
 ?>
+
+</section>
+</article>
+
+
+</body>
