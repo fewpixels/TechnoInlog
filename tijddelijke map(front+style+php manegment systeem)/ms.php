18600ae67cb262501e497d0ef372a63db133d55f
WimvdLinden <88454@mborijnland.nl>
2023-03-31 20:34:50 +0200

    management systeem update
    management backend

--- tijddelijke map(front+style+php manegment systeem)/ms.php
+++ tijddelijke map(front+style+php manegment systeem)/ms.php
@@ -10,8 +10,10 @@ $conn = mysqli_connect("localhost","root", '', 'inlogtechnolab'); //het moest du
 // }
 
 // Execute a SQL query to retrieve the data
-$sql = "SELECT * FROM my_table"; //verander tabel naam naar scan, loopt hier vast
-$result = mysqli_query($conn, $sql);
+$sql = "SELECT scan.userID,scan.inlogTijd,scan.uitlogTijd,scan.totaalTijd, CONCAT(users.voornaam,' ',users.tussenvoegsel,' ',users.achternaam) AS naam
+From users
+INNER JOIN scan ON users.id = scan.userID"; //verander tabel naam naar scan, loopt hier vast
+$result = mysqli_query($conn, $sql);// SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
 
 ?>
 <head>
@@ -21,22 +23,36 @@ $result = mysqli_query($conn, $sql);
     <img src="hoooo.png" alt="">
 
 
-<h1>welcome slavemaster</h1>
+<h1>welcome </h1>
 <table class = "sheet">
   <tr>
+  <th></th>
     <th>UserID</th>
     <th>Naam</th>
     <th>InlogTijd</th>
-	<th>UitlogTijd</th>
-	<th>TotaalTijd</th>
+	  <th>TotaalTijd</th>
+    <th>aantall checkins</th>
+    <th>aanpassen</th>
   </tr>
   
   <?php
   while ($row = mysqli_fetch_assoc($result)){
+
+
+   
+
+
+
+
     echo "<tr>";
-    echo "<td>" . $row["id"] . "</td>";
-    echo "<td>" . $row["naam"] . "</td>"; //dit ook veranderen
-    echo "<td>" . $row["email"] . "</td>"; //en dit ook
+    echo "<td>" . $row["userID"] . "</td>";
+
+    echo "<td>" . $row["naam"] . "</td>"; //hier moet een join komen met daar in de naam 
+
+    echo "<td>" . $row["inlogTijd"] . "</td>"; //en dit ook
+    echo "<td>" . $row["uitlogTijd"] . "</td>";
+    echo "<td>" . $row["totaalTijd"] . "</td>";
+    
     echo "</tr>";
   }
   ?>
