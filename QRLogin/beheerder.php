238236282db3e3de5e45dc980c20e340eeb8400e
Melissa Blankenburgh <melissablankenburgh@gmail.com>
2023-04-03 10:08:47 +0200

    aan de styling gewerkt
    + added logo
    + added triangle
    + main is now centered on more browsers

---
+++ QRLogin/beheerder.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'backend/classes/DBConfig.php'
+?>
+
+<head>
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+    <article>
+        <table>
+            <tr>
+                A
+            </tr>
+        </table>
+    </article>
+</body>
--- QRLogin/data_lookup.php
+++ QRLogin/data_lookup.php
@@ -2,30 +2,35 @@
     <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
+<img src="../image/Technolab Logo.png" alt="logo van technolab" id='logo'>
+<article id='mainmain'>
     
-<article id='main'>
-    <section id='text'>
+    <article id='main'>
+        <section id='text'>
 
-    <?php
+        <?php
 
-require_once 'backend/classes/timestamp.php';
+    require_once 'backend/classes/timestamp.php';
 
-$timeClass = new timeStamp();
+    $timeClass = new timeStamp();
 
-if(isset($_GET['data'])) {
-    // echo '<h1>beste ' . $_GET['data'] . ' je bent ingelogd!</h1>'; 
-    echo "<h1 id='output'> " . $timeClass->checkIn($_GET['data']) . " </h1>";
+    if(isset($_GET['data'])) {
+        // echo '<h1>beste ' . $_GET['data'] . ' je bent ingelogd!</h1>'; 
+        echo "<h1 id='output'> " . $timeClass->checkIn($_GET['data']) . " </h1>";
 
-     //header( "refresh:2; url=index.html" ); 
-}
+        //header( "refresh:2; url=index.html" ); 
+    }
 
-?>
+    ?>
 
-    </section>
-    <section id='filler'>
-        
-    </section>
+        </section>
+        <section id='filler'>
+            
+        </section>
+    </article>
 </article>
-
+<div id="tri">
+        
+    </div>
 
 </body>
