5668f58ecef07f096beb8b4bf820e7dfb39b40b9
fewpixels <74591245+fewpixels@users.noreply.github.com>
2023-04-12 13:19:54 +0200

    clean up data lookup file

--- QRLogin/data_lookup.php
+++ QRLogin/data_lookup.php
@@ -4,34 +4,22 @@
 <body>
 <img src="../image/Technolab Logo.png" alt="logo van technolab" id='logo'>
 <article id='mainmain'>
-    
     <article id='main'>
         <section id='text'>
-
         <?php
-
-    require_once 'backend/classes/timestamp.php';
-
-    $timeClass = new timeStamp();
-
-    if(isset($_GET['data'])) {
-        // echo '<h1>beste ' . $_GET['data'] . ' je bent ingelogd!</h1>'; 
-        echo "<h1 id='output'> " . $timeClass->checkIn($_GET['data']) . " </h1>";
-
-        header( "refresh:2; url=index.html" ); 
-    }
-
-    ?>
-
+            require_once 'backend/classes/timestamp.php';
+            $timeClass = new timeStamp();
+            if(isset($_GET['data'])) {
+                echo "<h1 id='output'> " . $timeClass->checkIn($_GET['data']) . " </h1>";
+                header( "refresh:2; url=index.html" ); 
+            }
+        ?>
         </section>
-        <section id='filler'>
-            
+        <section id='filler'>  
         </section>
     </article>
 </article>
 <img src="../image/Capture.PNG" alt="" id="otherimg">
 <div id="tri">
-        
     </div>
-
 </body>
