ec10be3c759f1bfe05fd9ae049558fdca3a888de
fewpixels <74591245+fewpixels@users.noreply.github.com>
2023-04-29 11:35:33 +0200

    added options for the scanner
    this could be potentially used to reduce the stress on the cpu

--- QRLogin/js/scanner.js
+++ QRLogin/js/scanner.js
@@ -1,20 +1,50 @@
-    function webScan(){  
-        let scanner = new Instascan.Scanner({ video: document.getElementById('scanner') });
-            scanner.addListener('scan', function (content) {
-            $(document).ready(function(){
-                window.location = './data_lookup.php?data='+content;
-            });
-            });
-            Instascan.Camera.getCameras().then(function (cameras) {
-                if (cameras.length > 0) {
-                    scanner.start(cameras[0]);
-                } else {
-                    console.error('No cameras found.');
-                }
-            }).catch(function (e) {
-                console.error(e);
-            });
+function webScan(){  
+    let opts = {
+      continuous: true,
+      video: document.getElementById('scanner'),
+      mirror: true,
+      captureImage: false,
+      backgroundScan: true,
+      refractoryPeriod: 5000,
+      scanPeriod: 1
     };
+    
+    let scanner = new Instascan.Scanner(opts);
+    
+    scanner.addListener('scan', function (content) {
+      $(document).ready(function(){
+        window.location = './data_lookup.php?data='+content;
+      });
+    });
+    
+    Instascan.Camera.getCameras().then(function (cameras) {
+      if (cameras.length > 0) {
+        scanner.start(cameras[0]);
+      } else {
+        console.error('No cameras found.');
+      }
+    }).catch(function (e) {
+      console.error(e);
+    });
+  };
+
+// function webScan(){  
+    //     let scanner = new Instascan.Scanner({ video: document.getElementById('scanner') });
+    //         scanner.addListener('scan', function (content) {
+    //         $(document).ready(function(){
+    //             window.location = './data_lookup.php?data='+content;
+    //         });
+    //         });
+    //         Instascan.Camera.getCameras().then(function (cameras) {
+    //             if (cameras.length > 0) {
+    //                 scanner.start(cameras[0]);
+    //             } else {
+    //                 console.error('No cameras found.');
+    //             }
+    //         }).catch(function (e) {
+    //             console.error(e);
+    //         });
+    // };
 
 
     //negeer wat hieronder staat
