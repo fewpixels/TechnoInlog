a8c0cfff4793d725b084fbe753d2b085419bfed1
fewpixels <74591245+fewpixels@users.noreply.github.com>
2023-05-31 13:46:54 +0200

    updated style and added buttons
    changed dashboard to have some buttons

--- QRLogin/ms.php
+++ QRLogin/ms.php
@@ -13,37 +13,38 @@ $scanList = $scans->getScans();
     <img src="../image/Technolab Logo.png" alt="" id='logo'>
 
 
-    
-<article id='container'>
-<h1>Scanlijst </h1>
-<a href="index.html"><button type="button">Terug</a></button>
-<table class = "sheet">
-  <tr>
-    <th>UserID</th>
-    <th>Naam</th>
-    <th>InlogTijd</th>
-    <th>UitlogTijd</th>
-	  <th>tijd verschil</th>
-    <th>aantal checkins</th>
-    <th>aantal checkouts</th>
-    <th>checkin indicator</th>
-  </tr>
-  
-  <?php
-  foreach($scanList as $singleScan){
-    echo "<tr>";
-    echo "<td>" . $singleScan->userID . "</td>";
-    echo "<td>" . $singleScan->naam . "</td>"; 
-    echo "<td>" . $singleScan->inlogTijd . "</td>"; //en dit ook
-    echo "<td>" . $singleScan->uitlogTijd . "</td>";
-    echo "<td>" .$singleScan->totaalTijd . "</td>";  
-    echo "</tr>";
-  }
-  ?>
- </table>
- </article>
- <div id="tri">
 
-    </div>
+  <article id='container'>
+  <h1>Scanlijst </h1>
+  <a href="index.html"><button type="button">Terug</a></button>
+
+    <table class="sheet">
+      <tr>
+        <th>UserID</th>
+        <th>Naam</th>
+        <th>InlogTijd</th>
+        <th>UitlogTijd</th>
+        <th>tijd verschil</th>
+        <th>Opties</th>
+      </tr>
+      
+      <?php
+      foreach($scanList as $singleScan){
+        echo "<tr>";
+        echo "<td>" . $singleScan->userID . "</td>";
+        echo "<td>" . $singleScan->naam . "</td>"; 
+        echo "<td>" . $singleScan->inlogTijd . "</td>"; //en dit ook
+        echo "<td>" . $singleScan->uitlogTijd . "</td>";
+        echo "<td>" . $singleScan->totaalTijd . "</td>";
+        echo "<td>" . "<a href='editUser.php?user=' ".$singleScan->userID."><button type='button'>Bewerken</a></button>";
+        echo "<td>" . "<a href=''><button type='button'>Verwijderen</a></button>";   
+        echo "</tr>";
+      }
+      ?>
+    </table>
+  </article>
+  <div id="tri">
+  </div>
 </body>
+
 
